4e57d219edcbc3812247afc1ae9fb32729199c9c
Lore0905 <lore.castelli4@gmail.com>
2021-11-16 19:23:32 +0100

    mailstone 2

--- MILESTONE 2/js/script.js
+++ MILESTONE 2/js/script.js
@@ -1,1 +1,23 @@
-alert("ciao il js funziona correttamente")
+let boxContainer = document.querySelector ('.list');
+for (let n = 0; n <= 100; n++) {
+
+    let scritta;
+
+    if ( n % 3 === 0 ) {
+        scritta = 'Fizz';
+    } 
+    else if ( n % 5 === 0 ) {
+        scritta = 'Buzz';
+    }
+    else{
+        scritta = n;
+    }
+
+
+    const items = `
+        <li class="box">${scritta}</li>
+    `;
+
+
+    boxContainer.innerHTML += items;
+}
--- MILESTONE 3 /js/script.js
+++
@@ -1,1 +0,0 @@
-alert("ciao il js funziona correttamente")
