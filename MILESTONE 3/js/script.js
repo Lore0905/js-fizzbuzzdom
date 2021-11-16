200cb6230ac56523274c6dc923ac4cb84894f77f
Lore0905 <lore.castelli4@gmail.com>
2021-11-16 23:39:04 +0100

    milestone 3

---
+++ MILESTONE 3/js/script.js
@@ -0,0 +1,26 @@
+let boxContainer = document.querySelector ('.list');
+for (let n = 0; n <= 100; n++) {
+
+    let scritta;
+
+    if ( (n % 3 === 0) && (n % 5 === 0) )   {
+        scritta = 'fizzbuzz';
+    } 
+    else if ( n % 3 === 0 ) {
+        scritta = 'Fizz';
+    } 
+    else if ( n % 5 === 0 ) {
+        scritta = 'Buzz';
+    }
+    else{
+        scritta = n;
+    }
+
+
+    const items = `
+        <li class="box">${scritta}</li>
+    `;
+
+
+    boxContainer.innerHTML += items;
+}
