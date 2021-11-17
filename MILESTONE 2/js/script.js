0e930e078e63a3d2d18a9bd19594c594fef077e7
Lore0905 <lore.castelli4@gmail.com>
2021-11-17 10:34:48 +0100

    done

--- MILESTONE 2/js/script.js
+++ MILESTONE 2/js/script.js
@@ -1,9 +1,14 @@
 let boxContainer = document.querySelector ('.list');
+
 for (let n = 0; n <= 100; n++) {
 
     let scritta;
 
-    if ( n % 3 === 0 ) {
+    if ( (n % 3 === 0) && (n % 5 === 0) )   {
+        scritta = 'fizzbuzz';
+        
+    }
+    else if ( n % 3 === 0 ) {
         scritta = 'Fizz';
     } 
     else if ( n % 5 === 0 ) {
--- MILESTONE 3/js/script.js
+++ MILESTONE 3/js/script.js
@@ -1,26 +1,32 @@
 let boxContainer = document.querySelector ('.list');
-for (let n = 0; n <= 100; n++) {
+for (let n = 1; n <= 100; n++) {
 
     let scritta;
+    let color;
 
     if ( (n % 3 === 0) && (n % 5 === 0) )   {
         scritta = 'fizzbuzz';
+        color = 'red';
     } 
     else if ( n % 3 === 0 ) {
         scritta = 'Fizz';
+        color = 'yellow';
     } 
     else if ( n % 5 === 0 ) {
         scritta = 'Buzz';
+        color = 'green';
     }
     else{
         scritta = n;
+        color = '';
     }
 
 
     const items = `
-        <li class="box">${scritta}</li>
+        <li class="box ${color}">${scritta}</li>
     `;
 
+    
 
     boxContainer.innerHTML += items;
 }
